26f207744ad403122f162eb7a93d2682b37d9f41
Aldhi ari kurniawan <aldhiarikurniawan@gmail.com>
2017-04-20 10:27:52 +0700

    Add files via upload
    Algoritma bfs untuk mapping classes game mmorpg 
    example comand => bfs(class, 'Fighter','Warlord') 
                                     dfs(class, 'Fighter','Warlord')

---
+++ depth_first_search.py
@@ -0,0 +1,56 @@
+classes =  { 'Novice':set(['Fighter','Hunter','SpellCaster']),
+             
+             'Fighter':set(['Warrior']),
+             'Warrior':set(['Fighter','Berserker','Paladins']),
+             'Berserker':set(['Warrior','Warlord']),
+             'Paladins':set(['Warrior','Templar',]),
+             'Warlord':set(['Berserker']),
+             'Templar':set(['Paladins']),
+             
+             'Hunter':set(['Archer']),
+             'Archer':set(['Hunter','Ranger','Assasin']),
+             'Ranger':set(['Archer','Sharpshooter']),
+             'Assasin':set(['Archer','Darkstalker']),
+             'Darkstalker':set(['Assasin']),
+             'SharpShooter':set(['Ranger']),
+             
+             'SpellCaster':set(['Mage']),
+             'Mage':set(['SpellCaster','Necromancer','Wizard']),
+             'Necromancer':set(['Mage','Demonologist']),
+             'Wizard':set(['Mage','Archmages']),
+             'Archmages':set(['Wizard']),
+             'Demonologist':set(['Necromancer'])
+         }
+
+def dfs(graf, mulai, tujuan):
+    stack = [[mulai]]
+    visited = set()
+
+    while stack:
+        
+        panjang_tumpukan = len(stack)-1
+        
+       
+        jalur = stack.pop(panjang_tumpukan)
+
+        
+        state = jalur[-1]
+
+        
+        if state == tujuan:
+            return jalur
+        
+        elif state not in visited:
+           
+            for cabang in graf.get(state, []): 
+                jalur_baru = list(jalur) 
+                jalur_baru.append(cabang) 
+                stack.append(jalur_baru)
+      
+            visited.add(state)
+
+
+        isi = len(stack)
+        if isi == 0:
+            print("Mapping tidak ditemukan karena Classes Berbeda")
+
